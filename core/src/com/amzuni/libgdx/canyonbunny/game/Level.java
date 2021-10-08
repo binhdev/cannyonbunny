fb44ac7d6e77d221cc5715df6714301d6a68d157
Thanh Binh <ttbinh@hueic.edu.vn>
2021-10-08 08:10:01 +0700

    game ui

--- core/src/com/amzuni/libgdx/canyonbunny/game/Level.java
+++ core/src/com/amzuni/libgdx/canyonbunny/game/Level.java
@@ -14,10 +14,10 @@ public class Level {
     public static final String TAG = Level.class.getName();
     public enum BLOCK_TYPE {
         EMPTY(0, 0, 0), // black
-            ROCK(0, 255, 0), // green
-            PLAYER_SPAWNPOINT(255, 255, 255), // white
-            ITEM_FEATHER(255, 0, 255), // purple
-            ITEM_GOLD_COIN(255, 255, 0); // yellow
+        ROCK(0, 255, 0), // green
+        PLAYER_SPAWNPOINT(255, 255, 255), // white
+        ITEM_FEATHER(255, 0, 255), // purple
+        ITEM_GOLD_COIN(255, 255, 0); // yellow
         private int color;
         private BLOCK_TYPE(int r, int g, int b) {
             color = r << 24 | g << 16 | b << 8 | 0xff;
